d5d797029191d85d256141e6d913600479b9e854
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-22 15:19:54 +0300

    fix:remove api key

--- src/redux/api/weatherApi.ts
+++ src/redux/api/weatherApi.ts
@@ -1,3 +1,4 @@
+import { API_KEY } from '@constants/api';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const weatherApi = createApi({
@@ -5,8 +6,7 @@ export const weatherApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.openweathermap.org/data/2.5/' }),
   endpoints: (builder) => ({
     getWeatherByLatLon: builder.query({
-      query: ({ lat, lon }) =>
-        `forecast?lat=${lat}&lon=${lon}&cnt=7&appid=99090fe6a8880314c12f83b028044c44`,
+      query: ({ lat, lon }) => `forecast?lat=${lat}&lon=${lon}&cnt=7&appid=${API_KEY}`,
     }),
   }),
 });
