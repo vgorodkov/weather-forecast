ee34280abb115d11ef7ba9b7efaca049d2afec7c
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-22 23:13:42 +0300

    chore: remove unnecessary imports

--- src/components/detailed_weather_screen/AdditionalInfo.tsx
+++ src/components/detailed_weather_screen/AdditionalInfo.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import { Label } from '@components/common/Label';
 import { colors } from '@constants/colors';
