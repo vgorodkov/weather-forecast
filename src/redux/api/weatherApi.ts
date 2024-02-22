36f1583bde9225ce0454d2e1ac9fb04334649b95
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-22 20:20:09 +0300

    chore: type rtk query response

--- src/redux/api/weatherApi.ts
+++ src/redux/api/weatherApi.ts
@@ -1,11 +1,12 @@
 import { API_KEY } from '@constants/api';
+import { WeatherResponse } from '@customTypes/weather';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const weatherApi = createApi({
   reducerPath: 'weatherApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://api.openweathermap.org/data/2.5/' }),
   endpoints: (builder) => ({
-    getWeatherByLatLon: builder.query({
+    getWeatherByLatLon: builder.query<WeatherResponse, { lat: number; lon: number }>({
       query: ({ lat, lon }) => `forecast?lat=${lat}&lon=${lon}&cnt=7&appid=${API_KEY}`,
     }),
   }),
