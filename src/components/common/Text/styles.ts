4d6a8f11e4a06f0deb0f45edca19e9add74f7948
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-22 20:18:31 +0300

    style: add color to text

--- src/components/common/Text/styles.ts
+++ src/components/common/Text/styles.ts
@@ -1,33 +1,39 @@
+import { colors } from '@constants/colors';
 import { FontFamily, FontVariant } from '@constants/font';
 import { TextStyle } from 'react-native';
 
 export const fontStyles: Record<FontVariant, TextStyle | TextStyle[]> = {
   [FontVariant.heading_1]: {
     fontFamily: FontFamily.RalewayBold,
+    color: colors.primary_text,
     fontSize: 32,
     lineHeight: 40,
   },
   [FontVariant.sub_heading]: {
     fontFamily: FontFamily.RalewayBold,
-    color: '#1B1639',
+    color: colors.primary_text,
     fontSize: 24,
     lineHeight: 40,
   },
   [FontVariant.body]: {
     fontFamily: FontFamily.RalewayRegular,
+    color: colors.primary_text,
     fontSize: 16,
   },
   [FontVariant.body_sb]: {
     fontFamily: FontFamily.RalewaySemiBold,
+    color: colors.primary_text,
     fontSize: 16,
   },
   [FontVariant.label]: {
     fontFamily: FontFamily.RalewayBold,
+    color: colors.primary_text,
     lineHeight: 16,
     fontSize: 16,
   },
   [FontVariant.label_medium]: {
     fontFamily: FontFamily.RalewayBold,
+    color: colors.primary_text,
     fontSize: 14,
   },
 };
