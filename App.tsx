4d6931d590bde50f91dbe39e1dce750506fb52d1
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-22 14:58:53 +0300

    chore: setup fonts, redux, navigation, safearea

--- App.tsx
+++ App.tsx
@@ -1,20 +1,60 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { FontFamily } from '@constants/font';
+import { useCallback } from 'react';
+
+import { MainStackNav } from '@navigation/MainStackNav';
+import { NavigationContainer } from '@react-navigation/native';
+import { Provider } from 'react-redux';
+import { store } from '@redux/store';
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  initialWindowMetrics,
+} from 'react-native-safe-area-context';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    [FontFamily.RalewayBlack]: require('@assets/fonts/Raleway-Black.ttf'),
+    [FontFamily.RalewayBold]: require('@assets/fonts/Raleway-Bold.ttf'),
+    [FontFamily.RalewaySemiBold]: require('@assets/fonts/Raleway-SemiBold.ttf'),
+    [FontFamily.RalewayMedium]: require('@assets/fonts/Raleway-Medium.ttf'),
+    [FontFamily.RalewayRegular]: require('@assets/fonts/Raleway-Regular.ttf'),
+    [FontFamily.RalewayThin]: require('@assets/fonts/Raleway-Thin.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+          <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+            <StatusBar style="auto" backgroundColor="" />
+            <MainStackNav />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
