af5ecbd8b7c6129b96d34c8f2a64620a1173210e
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-22 14:59:53 +0300

    feat: list of tracked weather

---
+++ src/components/all_weather_screen/TrackedWeatherList.tsx
@@ -0,0 +1,31 @@
+import { FlatList, StyleSheet } from 'react-native';
+import React from 'react';
+import { RootState } from '@redux/store';
+import { useSelector } from 'react-redux';
+import { WeatherCard } from './WeatherCard';
+
+import { TrackedWeather } from '@customTypes/weather';
+import { spacing } from '@constants/layout';
+
+export const TrackedWeatherList = () => {
+  const trackedWeather = useSelector((state: RootState) => state.weather.trackedWeather);
+
+  const renderItem = ({ item, index }: { item: TrackedWeather; index: number }) => {
+    return <WeatherCard key={index} item={item} />;
+  };
+
+  return (
+    <FlatList
+      data={trackedWeather}
+      renderItem={renderItem}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={styles.listContent}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  listContent: {
+    gap: spacing.dafault,
+  },
+});
