b288b944a4ad25f89df1ebe061bfc8774abbed8d
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-22 20:18:47 +0300

    refactor: move additional info to separate component

---
+++ src/components/detailed_weather_screen/AdditionalInfo.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Label } from '@components/common/Label';
+import { colors } from '@constants/colors';
+
+interface AdditionalInfoProps {
+  humidity: number;
+  wind: number;
+  clouds: number;
+}
+
+export const AdditionalInfo = ({ humidity, wind, clouds }: AdditionalInfoProps) => {
+  return (
+    <View style={styles.additionalInfoContainer}>
+      <Label
+        text={`${humidity} %`}
+        icon={require('@assets/icons/humidity.png')}
+        backgroundColor={colors.light_blue}
+      />
+      <Label
+        text={`${wind} km/h`}
+        icon={require('@assets/icons/wind.png')}
+        backgroundColor={colors.blue}
+      />
+      <Label
+        text={`${clouds} %`}
+        icon={require('@assets/icons/cloud.png')}
+        backgroundColor={colors.light_pink}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  additionalInfoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
