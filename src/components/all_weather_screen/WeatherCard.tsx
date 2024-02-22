a1c602d5bd43dd3186ec9fbe21aba9f80a719d90
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-02-22 20:18:20 +0300

    refactor: move mapWeatherData to utils

--- src/components/all_weather_screen/WeatherCard.tsx
+++ src/components/all_weather_screen/WeatherCard.tsx
@@ -9,7 +9,7 @@ import Animated, {
 
 import { Text } from '../common/Text';
 import { useGetWeatherByLatLonQuery } from '@redux/api/weatherApi';
-import { CustomWeatherData, TrackedWeather } from '@customTypes/weather';
+import { CustomWeatherData, TrackedWeather, WeatherResponse } from '@customTypes/weather';
 import { FontVariant } from '@constants/font';
 import { convertToCelsius } from 'utils/convertToCelsius';
 
@@ -18,6 +18,7 @@ import { useNavigation } from '@react-navigation/native';
 import { MainStackParamList, Route } from '@customTypes/navigation';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { Label } from '@components/common/Label';
+import { mapWeatherData } from 'utils/mapWeatherData';
 
 interface WeatherCardProps {
   item: TrackedWeather;
@@ -37,25 +38,11 @@ export const WeatherCard = ({ item }: WeatherCardProps) => {
     );
   }
 
-  const weatherData: CustomWeatherData = {
-    cityName: data?.city.name,
-    countryName: data?.city.country,
-    lat: data?.city.coord.lat,
-    lon: data?.city.coord.lon,
-    main: data?.list.map((item) => {
-      return {
-        date: item.dt_txt,
-        temp: item.main.temp,
-        temp_feels: item.main.feels_like,
-        temp_max: item.main.temp_max,
-        temp_min: item.main.temp_min,
-        clouds: item.clouds,
-        humidity: item.main.humidity,
-        wind: item.wind.speed,
-        description: item.weather,
-      };
-    }),
-  };
+  if (!data) {
+    return;
+  }
+
+  const weatherData = mapWeatherData(data);
   const currentWeather = weatherData.main[0];
   const onWeatherCard = () => {
     navigation.navigate(Route.DetailedWeather, {
@@ -88,7 +75,6 @@ export const WeatherCard = ({ item }: WeatherCardProps) => {
 const styles = StyleSheet.create({
   card: {
     backgroundColor: '#658ED930',
-
     padding: spacing.dafault,
     borderRadius: 8,
     gap: spacing.small,
